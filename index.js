6a62f3ee20ae1498c426584eafe06f1b0b9afc55
emnetdegafe <emnetdegafe@gmail.com>
2020-07-21 21:27:04 +0200

    price endpoint setup

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const { PORT } = require("./constants");
 const app = express();
+
 const bodyParserMiddleWare = express.json();
 app.use(bodyParserMiddleWare);
 
 
 
 
-const pricesRouter= require("./routers/price")
-app.use('/film/:price', pricesRouter)
+const pricesRouter= require("./routers/prices")
+app.use("/films", pricesRouter)
+// console.log('prices', pricesRouter)
 
-
-
+app.get("/", (req, res) => {
+  res.send("Hi from express");
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
---
+++ routers/prices.js
@@ -0,0 +1,48 @@
+const axios = require("axios");
+const { JSDOM } = require("jsdom");
+const { Router } = require("express")
+
+
+const router = new Router()
+
+
+
+
+router.get("/:eau", async (req, res) => {
+  async function getHtml() {
+    try {
+      const eau = req.params.eau
+      console.log('eau', eau)
+      
+      const result = await axios.get(
+        `https://www.amazon.nl/s?k=${eau}&__mk_nl_NL=%C3%85M%C3%85%C5%BD%C3%95%C3%91&ref=nb_sb_noss"`      );
+        const response = await axios.get(
+          `https://www.bol.com/nl/s/?searchtext=${eau}&searchContext=media_all&appliedSearchContextId=&suggestFragment=&adjustedSection=&originalSection=&originalSearchContext=&section=main&N=0&defaultSearchContext=media_all`
+          );
+                if(!result && !response ) {
+                  res.status(404).send("item not found")
+                } else {
+            // 5051889664123
+            
+            // console.log("bol", response.data);
+            
+            const dom = new JSDOM(result.data);
+            const dom1 = new JSDOM(response.data);
+            const eurosAmz = dom.window.document.getElementsByClassName(
+              "a-price-whole"
+              )[0].textContent;
+              const eurosBol = dom1.window.document.getElementsByClassName(
+                "promo-price"
+                )[0].textContent;
+                const bolPrice = eurosBol.replace(/\s/g,"");
+                console.log(eurosAmz, bolPrice);
+                // console.log(`${euros}, ${cents}`)
+                res.status(200).send({eurosAmz, bolPrice});
+              }
+    } catch (e) {
+      console.log("err", e);
+    }
+  }
+  getHtml();
+});
+module.exports = router
