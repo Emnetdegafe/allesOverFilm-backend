4301b183ba7d070482053466d12db1c8b1e89121
Emnetdegafe <emnetdegafe@gmail.com>
2020-07-28 12:53:34 +0200

    ParsInt - params Co-authored-by: Matias Garcia <matiasigarcia91@gmail.com>

--- routers/prices.js
+++ routers/prices.js
@@ -10,7 +10,7 @@ const router = new Router();
 router.get("/films/price/:eau", async (req, res) => {
   async function getHtml() {
     try {
-      const eau = req.params.eau;
+      const eau = parseInt(req.params.eau);
       console.log("eau", eau);
       const result = await axios.get(
         `https://www.amazon.nl/s?k=${eau}&__mk_nl_NL=%C3%85M%C3%85%C5%BD%C3%95%C3%91&ref=nb_sb_noss`
